da0c598781d201ade67105032aa6c1cb328c0368
NITESH KUMAR <176479517+DJmasTer5697@users.noreply.github.com>
2026-09-08 18:40:57 +0530

    Delete download_dataset.py

--- download_dataset.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-URL = "https://raw.githubusercontent.com/KalyanM45/Checking-Password-Strength-using-Machine-Learning/main/Password%20Strength.csv"
-OUT = "passwords.csv"
-
-r = requests.get(URL, timeout=60)
-r.raise_for_status()
-open(OUT, "wb").write(r.content)
-print(f"Saved {OUT}: {len(r.content)/1e6:.2f} MB")
